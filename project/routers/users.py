86f2cef92bcf7c3b734cc4093885e45a99d880e8
PauloParis <paul1963.1969@gmail.com>
2023-06-01 21:24:19 -0400

    login & cookie & reviews de un user autenticado

--- project/routers/users.py
+++ project/routers/users.py
@@ -7,6 +7,17 @@ from ..schemas import UserRequestModel
 from ..schemas import UserResponseModel
 
 
+from fastapi.security import HTTPBasicCredentials #login
+
+
+from fastapi import Response # cookies login
+from fastapi import Cookie # cookies login
+
+
+from typing import List
+from ..schemas import ReviewResponseModel
+
+
 router = APIRouter(prefix='/users')
 
 
@@ -28,3 +39,41 @@ async def create_user(user: UserRequestModel):
 
     #return UserResponseModel(id = user.id, username = user.username)
     return user  # se usa PeeweeGetterDict
+
+
+
+
+
+# --------- login ----------
+
+@router.post('/login', response_model = UserResponseModel)
+async def login(credentials: HTTPBasicCredentials, response: Response):
+    
+    user = User.select().where(User.username == credentials.username).first()
+
+    if user is None:
+        raise HTTPException(404, 'User not found')
+    
+    if user.password != User.create_password(credentials.password):
+        raise HTTPException(404, 'Password error')
+
+
+    response.set_cookie(key = 'user_id', value = user.id)
+
+    return user
+
+
+# traer las review de un user autenticado
+@router.get('/reviews', response_model = List[ReviewResponseModel])
+async def get_reviews(user_id: int = Cookie(None)): 
+    
+    user = User.select().where(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(404, 'User not found')
+    
+    return [ user_review for user_review in user.reviews ]
+
+
+
+
